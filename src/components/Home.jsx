4c08f43cb714f945d4f187d0666eda622c767510
Niels Garve <niels.garve@mailbox.org>
2018-02-25 14:17:37 +0100

    - extracted PageLayout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RichText from './RichText';
-import withPageLayout from './PageLayout';
+import withPageLayout from './withPageLayout';
 
 const Home = props => (
   <div>
